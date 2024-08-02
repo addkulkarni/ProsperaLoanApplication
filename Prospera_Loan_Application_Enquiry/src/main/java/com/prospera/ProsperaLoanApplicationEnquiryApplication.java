827c421c654a42ee7cb9519e09bb29b73cb9d9cf
pranali <pranalibhanage081@gmail.com>
2024-08-02 13:29:56 +0530

    changes in main-enquiry application

--- Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/ProsperaLoanApplicationEnquiryApplication.java
+++ Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/ProsperaLoanApplicationEnquiryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProsperaLoanApplicationEnquiryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("enquiry application");
 		SpringApplication.run(ProsperaLoanApplicationEnquiryApplication.class, args);
 	}
 
