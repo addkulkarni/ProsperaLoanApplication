030fe7e0ccb26f2cfbe984df007f602b58982d3b
addkulkarni <addkulkarni@gmail.com>
2024-08-05 17:39:38 +0530

    bug fixed

--- Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/serviceimpl/EnquiryServiceImpl.java
+++ Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/serviceimpl/EnquiryServiceImpl.java
@@ -129,7 +129,7 @@ public class EnquiryServiceImpl implements EnquiryServiceI{
 	@Override
 	public ResponseEntity<String> forwardToOE(int enquiryID)
 	{
-		Optional<Enquiry> o = er.findByIdAndEnquiryStatus(enquiryID, "Initiated");
+		Optional<Enquiry> o = er.findByEnquiryIDAndEnquiryStatus(enquiryID, "Initiated");
 		if(o.isPresent())
 		{
 			Enquiry e = o.get();
