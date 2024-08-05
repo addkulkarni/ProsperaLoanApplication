56567e596c17dc350af74a93b699c190cf3f0b37
addkulkarni <addkulkarni@gmail.com>
2024-08-05 12:28:32 +0530

    Minor changes

--- Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/servicei/EnquiryServiceI.java
+++ Prospera_Loan_Application_Enquiry/src/main/java/com/prospera/servicei/EnquiryServiceI.java
@@ -9,9 +9,9 @@ import com.prospera.model.Enquiry;
 
 public interface EnquiryServiceI {
 
-	ResponseEntity<Enquiry> updateEnquiry(int enquiryID, Enquiry e);
+	ResponseEntity<String> updateEnquiry(int enquiryID, Enquiry e);
 
-	ResponseEntity<Enquiry> addEnquiry(Enquiry e);
+	ResponseEntity<String> addEnquiry(Enquiry e);
 
 	ResponseEntity<String> deleteById(int enquiryID);
 
