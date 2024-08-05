8bccce0040f3eff1b9645cc20661062250e42993
pranali <pranalibhanage081@gmail.com>
2024-08-05 16:26:38 +0530

    adding  mail for cibil status

--- Prospera_Loan_Application_OE/src/main/java/com/prospera/serviceimpl/EnquiryServiceImpl.java
+++ Prospera_Loan_Application_OE/src/main/java/com/prospera/serviceimpl/EnquiryServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.prospera.model.Cibil;
@@ -17,6 +20,9 @@ public class EnquiryServiceImpl implements EnquiryServiceI
 {
 	@Autowired
 	EnquiryRepository er;
+	
+	@Autowired
+	private JavaMailSender sender;
 	
 	@Override
 	public ResponseEntity<String> calculateCibil(int enquiryID)
@@ -41,7 +47,33 @@ public class EnquiryServiceImpl implements EnquiryServiceI
 		en.setCibil(c);
 		er.save(en);
 		ResponseEntity<String> response=new ResponseEntity<String>("Your Cibil Score is "+randomnum,HttpStatus.OK);
+		 try
+		 {
+		SimpleMailMessage message=new SimpleMailMessage();
+		if(c.getCibilStatus().equals("Approved"))
+		{
+			message.setTo(en.getEmail());
+			message.setSubject("Congratulations " + en.getFirstName());
+			message.setText("Your cibil score is approved-" +c.getCibilscore()+"\n"+"Required documents for Further Process are following - \n"+" 1. Adharcard \n"+ "2. PanCard \n"+"3. 2 photo copy \n"+"4. Address Proof \n"+"5. Light bill");
+			
+		}
+		else
+		{
+			message.setTo(en.getEmail());
+			message.setSubject("Sorry " + en.getFirstName());
+			message.setText("Your Cibil Score Rejected");
+		}
+			sender.send(message);    
+		
+			
+		
+		 }
+		catch(MailException exception)
+		{
+			System.out.println("email is incorrect");
+		}
 		return response;
-	}
+	
 
+}
 }
