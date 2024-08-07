d4888319be13af3b4d84bf0ef445c46a3a3762a7
addkulkarni <addkulkarni@gmail.com>
2024-08-07 15:26:53 +0530

    Delete Prospera_Loan_Application_RE directory

--- Prospera_Loan_Application_RE/src/main/java/com/prospera/servicei/CustomerServiceI.java
+++
@@ -1,9 +0,0 @@
-package com.prospera.servicei;
-
-import com.prospera.model.Customer;
-import com.prospera.model.Enquiry;
-
-public interface CustomerServiceI
-{
-	void saveData(Enquiry e, Customer c);
-}
